70d58bcc184f3642bbef236947825b919845e6d8
施小雨-QA外包-晖致 <xiaoyu.shi@viatris.com>
2025-07-24 20:40:59 +0800

    bug 修复

--- test/bussiness/全局智能定投管理/test_redeem.py
+++ test/bussiness/全局智能定投管理/test_redeem.py
@@ -26,7 +26,7 @@ def test_redeem():
     # 打印测试开始信息
     logger.info("开始测试 redeem 函数")
 
-    detail_response = getPlanDetailPro('3ec795d8e4804d45a0e0e3d1b3fe0064', DEFAULT_USER)
+    detail_response = getPlanDetailPro('a0a19105487e4baea6063ebcfc7b9542', DEFAULT_USER)
     # 调用函数进行止盈测试
     result = redeem(DEFAULT_USER, detail_response.Data)
     

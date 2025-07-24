0ea2a5f7d912ef72be5c3022a3029b6f034bc993
施小雨-QA外包-晖致 <xiaoyu.shi@viatris.com>
2025-07-24 14:37:11 +0800

    现金阀值改成了100w

--- src/bussiness/全局智能定投处理/redeem.py
+++ src/bussiness/全局智能定投处理/redeem.py
@@ -176,7 +176,7 @@ def redeem(user: User, plan_detail: FundPlanDetail) -> bool:
         logger.info(f"银行卡余额：{CurrentRealBalance}")
         
         #检查银行卡余额,小于30万，且收益大于1.0，立即卖出费率为0的份额
-        if estimated_profit_rate > 1.0 and CurrentRealBalance < 500000 and fund_type == '000' and fund_info.estimated_change != 0.0:
+        if estimated_profit_rate > 1.0 and CurrentRealBalance < 1000000 and fund_type == '000' and fund_info.estimated_change != 0.0:
             logger.info(f"{customer_name}的止盈操作开始：余额:{CurrentRealBalance},基金{fund_name}{fund_code}(类型:{fund_type})预估收益{estimated_profit_rate},实际止盈点:1.0.")
             sell_0_fee_shares(user,sub_account_no,fund_code,shares)
             return True

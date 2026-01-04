f1a93b73a46110f1f7d23143e94aff5d9ac6b4c9
施小雨-QA外包-晖致 <xiaoyu.shi@viatris.com>
2026-01-04 14:17:49 +0800

    3%止盈的时候基金排名rank_100>80

--- src/bussiness/全局智能定投处理/redeem.py
+++ src/bussiness/全局智能定投处理/redeem.py
@@ -272,7 +272,7 @@ def redeem(user: User, plan_detail: FundPlanDetail) -> bool:
             logger.info(f"指数基金余额条件检查：预估收益{estimated_profit_rate}，余额{CurrentRealBalance}，基金类型{fund_type}，估值变化{fund_info.estimated_change}")
             
         #检查银行卡余额,小于50万，且收益大于3.0，且投资次数小于5.0次，立即卖出费率为0的份额
-        if estimated_profit_rate > 3.0 and CurrentRealBalance < BANK_BALANCE_THRESHOLD and fund_type in ['001','002'] and rank_100 is not None and times < 5.0 and estimated_change > 0.5:
+        if estimated_profit_rate > 3.0 and CurrentRealBalance < BANK_BALANCE_THRESHOLD and fund_type in ['001','002'] and rank_100 is not None and rank_100 > 80 and times < 5.0 and estimated_change > 0.5:
             logger.info(f"{customer_name}的止盈操作开始：余额:{CurrentRealBalance},阈值:{BANK_BALANCE_THRESHOLD},基金{fund_name}{fund_code}(类型:{fund_type})预估收益{estimated_profit_rate},实际止盈点:3.0, 100日排名:{rank_100},投资次数:{times}.")
             sell_usable_non_zero_fee_shares(user,sub_account_no,fund_code,shares)
             return True
